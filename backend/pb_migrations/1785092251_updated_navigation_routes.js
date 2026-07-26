482ca2299d426403e2de86cbebe5b9243796ca25
Marycarmen <marycarmen.rivo@gmail.com>
2026-07-26 13:13:06 -0600

    Pcoketbase migrations

---
+++ backend/pb_migrations/1785092251_updated_navigation_routes.js
@@ -0,0 +1,28 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((app) => {
+  const collection = app.findCollectionByNameOrId("pbc_1151979533")
+
+  // add field
+  collection.fields.addAt(5, new Field({
+    "help": "",
+    "hidden": false,
+    "id": "number4113142680",
+    "max": null,
+    "min": null,
+    "name": "order",
+    "onlyInt": false,
+    "presentable": false,
+    "required": true,
+    "system": false,
+    "type": "number"
+  }))
+
+  return app.save(collection)
+}, (app) => {
+  const collection = app.findCollectionByNameOrId("pbc_1151979533")
+
+  // remove field
+  collection.fields.removeById("number4113142680")
+
+  return app.save(collection)
+})
