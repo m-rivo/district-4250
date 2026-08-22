ff9142979a8b00f0daa0a1ab6ccc31525fcb07df
Marycarmen <marycarmen.rivo@gmail.com>
2026-08-22 14:26:58 -0600

    Added loading skeletons to all pages

---
+++ frontend/app/[locale]/(dashboard)/profile/[[...id]]/loading.tsx
@@ -0,0 +1,16 @@
+import { Skeleton } from "@/components/ui/skeleton";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+
+export default function Loading() {
+  return (
+    <Card className="w-full max-w-xs">
+      <CardHeader>
+        <Skeleton className="h-4 w-2/3" />
+        <Skeleton className="h-4 w-1/2" />
+      </CardHeader>
+      <CardContent>
+        <Skeleton className="aspect-video w-full" />
+      </CardContent>
+    </Card>
+  );
+}
